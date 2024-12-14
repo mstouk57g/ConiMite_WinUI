70ea709e74164fa7c3cc139cd8b81d1a7b6df9c7
mstouk57g <KissLmisu@outlook.com>
2024-12-14 10:16:16 +0800

    实现绑定

--- ViewModelBinding/ViewModels/MainViewModel.cs
+++ ViewModelBinding/ViewModels/MainViewModel.cs
@@ -1,10 +1,18 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Diagnostics;
 
 namespace ViewModelBinding.ViewModels;
 
 public partial class MainViewModel : ObservableRecipient
 {
-    public MainViewModel()
+    [ObservableProperty]
+    private string displayText = "初始文本";
+
+    [RelayCommand]
+    private void ChangeText()
     {
+        Trace.WriteLine("nm");
+        DisplayText = "按钮被点击了";
     }
 }
